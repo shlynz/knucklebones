717c4d312c59c6e36d635f608f06c3c75691d151
Shlynz <75536038+shlynz@users.noreply.github.com>
2022-09-11 15:26:53 +0200

    Revamped turn handling and added the destroying of opponents dice mechanic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,25 @@
-const Board = ({board, setBoard, isTurn, diceResult, remote = false}) => {
-    
+import Dice from './Dice';
 
+const Board = ({board, setNextMove, isTurn, diceResult, remote = false}) => {
+    
+    // if a field is getting clicked, check if its not opponents board, its your turn and the field isn't already occupied
     const handleClick = (fieldId) => {
         if(remote || !isTurn) return;
         if(board[fieldId]) return;
-        const newBoard = [...board];
-        newBoard[fieldId] = diceResult;
-        console.log(newBoard);
-        setBoard(newBoard);
+        setNextMove({fieldId, diceResult});
     }
 
     return(
         <div className='board'>
             {
+                // map the board to a grid of die
                 board.map((field, index) => 
                     <a 
                         className='field'
                         key={remote ? `remoteField${index}` : `field${index}`}
                         onClick={() => handleClick(index)}
                     >
-                        {field}
+                        <Dice diceResult={field}/>
                     </a>
                 )
             }
--- src/components/Game.js
+++ src/components/Game.js
@@ -7,27 +7,53 @@ const Game = ({connection, isTurn, setIsTurn}) => {
     const generateBoard = () => Array(9).fill(null);
     const [myBoard, setMyBoard] = useState(generateBoard());
     const [remoteBoard, setRemoteBoard] = useState(generateBoard());
-    const [diceResult, setDiceResult] = useState(6);
+    const [diceResult, setDiceResult] = useState();
+    const [nextMove, setNextMove] = useState();
 
+    // rolls a dice with the specified number of sides
     const rollDice = (sides) => setDiceResult(Math.floor(Math.random() * sides) + 1);
 
+    // do a move for the specified board and return the new board states
+    const doMove = (movedBoard, setMovedBoard, otherBoard, setOtherBoard, move, setMove) => {
+        const newOtherBoard = otherBoard.map((value, index) => (value === move.diceResult && index % 3 === move.fieldId % 3) ? null : value);
+        setOtherBoard(newOtherBoard);
+
+        const newMovedBoard = [...movedBoard];
+        newMovedBoard[move.fieldId] = move.diceResult;
+        setMovedBoard(newMovedBoard);
+
+        setMove(null);
+
+        return {movedBoard: newMovedBoard, otherBoard: newOtherBoard};
+    };
+
+    // if data is received, update the boards and start local turn
     connection.on('data', data => {
-        setRemoteBoard(data)
+        setMyBoard(data.otherBoard);
+        setRemoteBoard(data.movedBoard);
         rollDice(6);
         setIsTurn(true);
     });
 
+    // do once on load
     useEffect(() => {
+        if(isTurn) rollDice(6);
+    }, [])
+
+    // if a move is made, update the boards, send them to peer and end local turn
+    useEffect(() => {
+        if(!nextMove) return;
+        const {movedBoard, otherBoard} = doMove(myBoard, setMyBoard, remoteBoard, setRemoteBoard, nextMove, setNextMove);
+        connection.send({movedBoard, otherBoard});
         setDiceResult(null);
-        connection.send(myBoard);
         setIsTurn(false);
-    }, [myBoard]);
+    }, [nextMove]);
 
     return(
         <div className='game'>
             This is where the game is played
-            <Board board={remoteBoard} setBoard={setRemoteBoard} remote={true} />
-            <Board board={myBoard} setBoard={setMyBoard} isTurn={isTurn} diceResult={diceResult} />
+            <Board board={remoteBoard} remote={true} />
+            <Board board={myBoard} setNextMove={setNextMove} isTurn={isTurn} diceResult={diceResult} />
             <Dice diceResult={diceResult} />
         </div>
     )
