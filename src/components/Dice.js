f1f9c00fbdac26bccd38ec3f65e03b4048f139eb
Shlynz <75536038+shlynz@users.noreply.github.com>
2022-09-11 12:48:23 +0200

    Dice now shows dice face instead of just the number

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,7 +1,49 @@
 const Dice = ({diceResult}) => {
 
-    return(<a className='dice'>{diceResult}</a>)
+    const faces = [
+        [
+            false, false, false,
+            false, true, false,
+            false, false, false
+        ],
+        [
+            false, false, true,
+            false, false, false,
+            true, false, false
+        ],
+        [
+            false, false, true,
+            false, true, false,
+            true, false, false
+        ],
+        [
+            true, false, true,
+            false, false, false,
+            true, false, true
+        ],
+        [
+            true, false, true,
+            false, true, false,
+            true, false, true
+        ],
+        [
+            true, false, true,
+            true, false, true,
+            true, false, true
+        ]
+    ]
 
+    const getDiceFace = (nr) => {
+        if(!nr) return;
+        const face = nr-1;
+        return faces[face].map((isPip, pipIndex) => {
+            return <p className={isPip ? 'pip' : 'filler-pip' } key={pipIndex} />
+        });
+    }
+
+    if(diceResult && diceResult <= faces.length){
+        return <div className='dice'>{getDiceFace(diceResult)}</div>
+    }
 }
 
 export default Dice;
