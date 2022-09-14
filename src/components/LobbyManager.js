1f34a3bff13cd0a18b7b3ef5083a0292e80d7a59
Shlynz <75536038+shlynz@users.noreply.github.com>
2022-09-14 17:06:33 +0200

    Cancels both sides when one loses connection and shortened lobby id

--- src/components/LobbyManager.js
+++ src/components/LobbyManager.js
@@ -3,13 +3,24 @@ import { useState } from 'react';
 
 const LobbyManager = ({setConnection, setIsTurn}) => {
 
-    let [peerId, setPeerId] = useState('');
-    let [connectionId, setConnectionId] = useState('');
+    const idPrefix = 'shlynz-knucklebones-';
+    const [peerId, setPeerId] = useState('');
+    const [connectionId, setConnectionId] = useState('');
 
-    let createGame = () => {
+    const getNewId = () => {
+        // I know this is a bad way to 'generate IDs' but for the scope of this project it'll do
+        // placed into a function to make a potential change in the future easier :)
+        return idPrefix + Date.now().toString().slice(-6);
+    }
+
+    const resetToHomepage = () => {
+        window.location.reload();
+    }
+
+    const createGame = () => {
         if(peerId) return;
         setPeerId('Awaiting ID')
-        const peer = new Peer();
+        const peer = new Peer(getNewId());
         peer.on('open', (id) => {
             console.log('My peer ID is: ' + id);
             setPeerId(id);
@@ -17,20 +28,22 @@ const LobbyManager = ({setConnection, setIsTurn}) => {
                 console.log('connection received');
                 setConnection(conn);
                 setIsTurn(true);
+                conn.on('close', resetToHomepage);
             });
         });
     }
 
-    let joinGame = () => {
+    const joinGame = () => {
         const peer = new Peer();
         peer.on('open', (id) => {
             console.log('My PeerID ' + id);
             console.log('connecting to ' + connectionId);
-            let conn = peer.connect(connectionId);
+            let conn = peer.connect(idPrefix + connectionId);
             conn.on('open', () => {
                 console.log('connection opened...');
                 setConnection(conn);
             });
+            conn.on('close', resetToHomepage);
         });
     }
 
@@ -40,7 +53,7 @@ const LobbyManager = ({setConnection, setIsTurn}) => {
             <label htmlFor='lobby-id'>Lobby ID: </label>
             <input id='lobby-id' onChange={event => setConnectionId(event.target.value)} value={connectionId}/><br/>
             <label htmlFor='own-id'>Your ID: </label>
-            <input id='own-id' value={peerId} readOnly /><br/>
+            <input id='own-id' value={peerId.slice(-6)} readOnly /><br/>
             <a className='button' onClick={createGame}>Create Game</a><br/>
             <a className='button' onClick={joinGame}>Join Game</a><br/>
         </div>
